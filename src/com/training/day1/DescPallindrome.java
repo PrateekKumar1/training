ab29d746045f7c2eec92d4b1f14f48cb383bc63b
trainee <prateekkr1295@gmail.com>
2018-04-16 20:29:10 +0530

    first commit day1 programs

--- src/com/training/day1/DescPallindrome.java
+++ src/com/training/day1/DescPallindrome.java
@@ -3,9 +3,42 @@ package com.training.day1;
 import java.util.Scanner;
 
 public class DescPallindrome {
-
+	public static void checkPalindrome(String s)
+	  {
+	    // reverse the given String
+	    String reverse = new StringBuffer(s).reverse().toString();
+	 
+	    // check whether the string is palindrome or not
+	    if (s.equals(reverse))
+	    {  
+	    	//System.out.println("Yes");
+	    	
+	    	
+	    
+	    	
+	    		
+	    		
+	    		List<String> myList = new ArrayList<String>();
+	    		myList.add(reverse);
+	    		Collections.sort(myList, Collections.reverseOrder());
+	    		
+	    		
+	    		for(String str: myList){
+	    			System.out.println(str);
+	    	   }
+	    	
+	    	   //   Iterator iter1 = myList.iterator();
+	    	    //  while (iter1.hasNext()) {
+	    	    //     System.out.println(iter1.next());
+	    	      }
+	    	
+	    	
+	    	
+	    
+	    	System.out.println();
+	    }
 	public static void main(String[] args) {
-		String original, reverse = ""; // Objects of String class
+		/*String original, reverse = ""; // Objects of String class
 	      Scanner in = new Scanner(System.in);
 	      System.out.println("Enter 10 inputs");
 	      String[] arr=new String[10];
@@ -20,7 +53,11 @@ public class DescPallindrome {
 	         System.out.println("Entered string is a palindrome.");
 	      else
 	         System.out.println("Entered string isn't a palindrome.");
-
+*/
+		// checkPalindrome("malayalam");
+	    checkPalindrome("GeeksforGeeks");
+	    checkPalindrome("121");
+	    checkPalindrome("12321");
 	}
 
 }
