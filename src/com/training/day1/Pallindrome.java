2db036e25840dc35a24b73717bda3e9b01cd768a
trainee <prateekkr1295@gmail.com>
2018-04-16 14:43:57 +0530

    first commit day1programs

---
+++ src/com/training/day1/Pallindrome.java
@@ -0,0 +1,34 @@
+package com.training.day1;
+import java.io.*;
+import java.util.*;
+
+public class Pallindrome {
+
+	public static void main(String[] args) throws IOException, NumberFormatException{
+		Scanner rd=new Scanner(System.in);
+		int n,r,sum=0;
+		int vr; //initializing temporary variable for input storage
+		
+		System.out.print("Enter the number \t");
+		n=rd.nextInt();
+		vr=n; //passing the initial value to temporary variable
+		while(n>0)
+		{
+			r=n%10;
+			sum=(sum*10)+r;
+			n=n/10;
+		}
+		if(vr==sum) //Equating initial number with the reversed number
+		{
+			
+			System.out.print("true");
+		}
+		else
+		{
+					System.out.println("false");
+		}
+	
+		
+	}
+
+}
---
+++ src/com/training/day1/StringReverse.java
@@ -0,0 +1,12 @@
+package com.training.day1;
+import java.*;
+public class StringReverse {
+
+	public static void main(String[] args) {
+		String str="Let's take Leet contest";
+		String reverse=new StringBuffer(str).reverse().toString();
+		System.out.println(reverse);
+
+	}
+
+}
