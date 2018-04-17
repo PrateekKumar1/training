26b315a8b67b1763cc876e1ca7aef813c14ca924
trainee <prateekkr1295@gmail.com>
2018-04-17 19:06:23 +0530

    17Apr day2 programs

--- src/com/training/day2/LambdaExpression.java
+++ src/com/training/day2/LambdaExpression.java
@@ -20,16 +20,20 @@ import java.util.*;
  */
 
 public class LambdaExpression {
-
+interface EvenOdd {
+	void check(int a[]);
+}
 	public static void main(String[] args) {
-		int i,n;
+		int i,n,ch;
 		Scanner rd=new Scanner(System.in);
 		System.out.print("Enter the range\t");
 		n=rd.nextInt();
 		int[] arr=new int[n];
+		System.out.print("Enter elements");
 		for(i=0;i<n;i++) {
 			arr[i]=rd.nextInt();
 		}
+		
 	}
 
 }
