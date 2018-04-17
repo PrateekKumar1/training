f61cf1e91fd1d672e6576a08ebc0efb1fa54eba9
trainee <prateekkr1295@gmail.com>
2018-04-17 12:19:12 +0530

    17Apr day2 programs modified reorganize spring

--- src/com/training/day2/ReorganizeString.java
+++ src/com/training/day2/ReorganizeString.java
@@ -4,6 +4,7 @@
 package com.training.day2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
@@ -22,13 +23,14 @@ public class ReorganizeString {
 	
 	public static void main(String[] args) {
 		 String input, output;
+		 int d;
 		 List<Character> characters = new ArrayList<>();
 
 	        Scanner scanner = new Scanner(System.in);
 
-	        System.out.print("Please type sentace to encode: ");
+	        System.out.print("Please enter the string: ");
 	        input = scanner.nextLine();
-
+	        
 	        for(int i = 0; i < input.length(); i++) {
 	            characters.add(input.charAt(i)); 
 	        }
@@ -36,6 +38,10 @@ public class ReorganizeString {
 	        while(itr.hasNext()){  
 	         System.out.print(itr.next());  
 	        }  
+	        System.out.println();  
+	        d=input.length()-2;
+	        Collections.rotate(characters,-d) ;
+	        System.out.print(characters);
 	}
 
 }
