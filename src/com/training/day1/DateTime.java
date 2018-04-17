4f5192cd3c6c44521765b3e8ebc1582c704b09ee
trainee <prateekkr1295@gmail.com>
2018-04-17 09:36:17 +0530

    day1 programs

--- src/com/training/day1/DateTime.java
+++ src/com/training/day1/DateTime.java
@@ -9,37 +9,22 @@ public class DateTime
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-         
         System.out.println("Enter start date in dd-MM-yyyy/HH:mm:ss format :");
-         
         String s1 = sc.next();
-         
         System.out.println("Enter end date in dd-MM-yyyy/HH:mm:ss format :");
-         
         String s2 = sc.next();
-         
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy/HH:mm:ss");
-         
         try
         {
             Date startDate = formatter.parse(s1);
-             
             Date endDate = formatter.parse(s2);
-             
-            long diffInMilliSec = endDate.getTime() - startDate.getTime();
-             
-            long seconds = (diffInMilliSec / 1000) % 60;
-             
-            long minutes = (diffInMilliSec / (1000 * 60)) % 60;
-             
-            long hours = (diffInMilliSec / (1000 * 60 * 60)) % 24;
-             
-            long days = (diffInMilliSec / (1000 * 60 * 60 * 24)) % 365;
-             
-            long years =  (diffInMilliSec / (1000l * 60 * 60 * 24 * 365));
-             
+            long diffInMilliSec = endDate.getTime() - startDate.getTime(); //calculating difference between dates
+            long seconds = (diffInMilliSec / 1000) % 60;  //seconds conversion
+            long minutes = (diffInMilliSec / (1000 * 60)) % 60; //minutes conversion
+            long hours = (diffInMilliSec / (1000 * 60 * 60)) % 24; //hour conversion
+            long days = (diffInMilliSec / (1000 * 60 * 60 * 24)) % 365; //days
+            long years =  (diffInMilliSec / (1000l * 60 * 60 * 24 * 365)); //years
             System.out.print("Difference is ---> ");
-             
             System.out.println(years+" years, "+days+" days, "+hours+" hours, "+minutes+" minutes, "+seconds+" seconds");
         }
         catch (ParseException e)
