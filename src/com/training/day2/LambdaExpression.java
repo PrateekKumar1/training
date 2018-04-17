40107fc8bb24178f432385953e2780dad9d31a3b
trainee <prateekkr1295@gmail.com>
2018-04-17 15:20:43 +0530

    17Apr day2 programs

--- src/com/training/day2/LambdaExpression.java
+++ src/com/training/day2/LambdaExpression.java
@@ -5,7 +5,7 @@ package com.training.day2;
 import java.util.*;
 
 /**
- * ********************************************************************************************************************** 
+ * ****************************************************************************************************************** 
  *	Program name        : 	Lambda Expression
  *	Program Description :	Write the following methods that return a lambda expression performing a specific action-
  *                          PerformOperation isOdd() : The lambda expression must return true if a number is odd or 
@@ -15,18 +15,21 @@ import java.util.*;
  *                          PerformOperation isPalindrome() : The lambda expression must return true if a number is a
  *                          palindrome or false if it is not. 
  *                         
- *  *********************************************************************************************************************
+ *  *****************************************************************************************************************
  *
  */
 
 public class LambdaExpression {
 
 	public static void main(String[] args) {
-		int n;
+		int i,n;
 		Scanner rd=new Scanner(System.in);
 		System.out.print("Enter the range\t");
 		n=rd.nextInt();
-		
+		int[] arr=new int[n];
+		for(i=0;i<n;i++) {
+			arr[i]=rd.nextInt();
+		}
 	}
 
 }
---
+++ src/com/training/day2/PhoneBook.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.training.day2;
+
+/**
+ * @author trainee
+ *
+ */
+public class PhoneBook {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
