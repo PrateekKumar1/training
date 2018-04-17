aae33ae6dbd709d4ed91200c1c55adfb72628814
trainee <prateekkr1295@gmail.com>
2018-04-17 16:34:04 +0530

    17Apr day2 programs

--- src/com/training/day2/PhoneBook.java
+++ src/com/training/day2/PhoneBook.java
@@ -2,19 +2,41 @@
  * 
  */
 package com.training.day2;
-
+import java.util.*;
+import java.io.*;
 /**
- * @author trainee
+ * ***************************************************************************************************************************** 
+ *	Program name        : 	Phone Book
+ *	Program Description :	You are given a phone book that consists of people's names and their  phone number. After that 
+ *                         you will be given some person's name as query. For each query, print the phone number of that person.
+ *                         Create a menu:
+ *                         1.add data and sort in ascending order
+ *                         2.remove data
+ *                         3.search by name
+ *                         4.search by number
+ *                         5.search by sr no
+ *                         Conditions:
+ *                         A person's name consists of only lower-case English letters and it may be in the format 'first-name
+ *                         last-name' or in the format 'first-name'.'first-name' is compulsory.
+ *                         Each phone number has exactly 10 digits without any leading zeros.
+ *  ****************************************************************************************************************************
  *
  */
+
 public class PhoneBook {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner rd=new Scanner(System.in);
+		int n=0,ch;
+		HashMap<Integer,String> pb=new HashMap<Integer,String>();
+		System.out.print("\t\tPhone Book\n1.add data and sort in ascending order\n2.remove data\n3.search by name\n4.search by number\r\n5.search by sr no");
+		System.out.print("Enter your choice\t");
+		ch=rd.nextInt();
+		switch(ch)
+		{
+		case 1:
+		}
 	}
 
 }
