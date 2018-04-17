bee819a153296b87923961b89ffecde959105335
trainee <prateekkr1295@gmail.com>
2018-04-17 16:58:27 +0530

    17Apr day2 programs

--- src/com/training/day2/PhoneBook.java
+++ src/com/training/day2/PhoneBook.java
@@ -22,14 +22,24 @@ import java.io.*;
  *  ****************************************************************************************************************************
  *
  */
-
+class Directory {    
+int id;    
+String fname,lname,phoneno;    
+  
+public Directory(int id, String fname, String lname, String phoneno) {    
+    this.id = id;    
+    this.fname = fname;    
+    this.lname = lname;    
+    this.phoneno = phoneno;    
+    }    
+}    
 public class PhoneBook {
 
 	
 	public static void main(String[] args) {
 		Scanner rd=new Scanner(System.in);
 		int n=0,ch;
-		HashMap<Integer,String> pb=new HashMap<Integer,String>();
+		HashMap<Integer,Directory> pb=new HashMap<Integer,Directory>();//creating map of directory
 		System.out.print("\t\tPhone Book\n1.add data and sort in ascending order\n2.remove data\n3.search by name\n4.search by number\r\n5.search by sr no");
 		System.out.print("Enter your choice\t");
 		ch=rd.nextInt();
