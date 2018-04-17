de228f836e61b8601e30ed1105e10ee6faf23edf
trainee <prateekkr1295@gmail.com>
2018-04-17 15:13:13 +0530

    17Apr day2 programs

---
+++ src/com/training/day2/LambdaExpression.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.training.day2;
+import java.util.*;
+
+/**
+ * ********************************************************************************************************************** 
+ *	Program name        : 	Lambda Expression
+ *	Program Description :	Write the following methods that return a lambda expression performing a specific action-
+ *                          PerformOperation isOdd() : The lambda expression must return true if a number is odd or 
+ *                          false if it is even.
+ *                          PerformOperation isPrime() : The lambda expression must return true if a number is prime
+ *                          or false if it is composite.
+ *                          PerformOperation isPalindrome() : The lambda expression must return true if a number is a
+ *                          palindrome or false if it is not. 
+ *                         
+ *  *********************************************************************************************************************
+ *
+ */
+
+public class LambdaExpression {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner rd=new Scanner(System.in);
+		System.out.print("Enter the range\t");
+		n=rd.nextInt();
+		
+	}
+
+}
