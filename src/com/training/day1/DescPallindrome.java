924c15d6315f02a8334cf0defd748f0f466dfc17
trainee <prateekkr1295@gmail.com>
2018-04-17 09:46:40 +0530

    day1 programs

--- src/com/training/day1/DescPallindrome.java
+++ src/com/training/day1/DescPallindrome.java
@@ -16,11 +16,6 @@ public class DescPallindrome {
 	    {  
 	    	//System.out.println("Yes");
 	    	
-	    	
-	    
-	    	
-	    		
-	    		
 	    		List<String> myList = new ArrayList<String>();
 	    		myList.add(reverse);
 	    		Collections.sort(myList, Collections.reverseOrder());
@@ -30,9 +25,7 @@ public class DescPallindrome {
 	    			System.out.println(str);
 	    	   }
 	    	
-	    	   //   Iterator iter1 = myList.iterator();
-	    	    //  while (iter1.hasNext()) {
-	    	    //     System.out.println(iter1.next());
+	    	
 	    	      }
 	    	
 	    	
@@ -41,24 +34,8 @@ public class DescPallindrome {
 	    	System.out.println();
 	    }
 	public static void main(String[] args) {
-		/*String original, reverse = ""; // Objects of String class
-	      Scanner in = new Scanner(System.in);
-	      System.out.println("Enter 10 inputs");
-	      String[] arr=new String[10];
-	      original = in.nextLine();
-	 
-	      int length = original.length();
-	 
-	      for ( int i = length - 1; i >= 0; i-- )
-	         reverse = reverse + original.charAt(i);
-	 
-	      if (original.equals(reverse))
-	         System.out.println("Entered string is a palindrome.");
-	      else
-	         System.out.println("Entered string isn't a palindrome.");
-*/
-		// checkPalindrome("malayalam");
-	    checkPalindrome("Java");
+		
+	  //  checkPalindrome("Java");
 	    checkPalindrome("121");
 	    checkPalindrome("12321");
 	}
