d37b6a53131aa66745c3ec70ec517cb33a8a4dd5
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-19 10:57:49 +0530

    April16 PMD Maven and Day2 Final Modifications

---
+++ src/com/training/April16testingPMDMaven/src/test/java/com/training/April16testing/FileIOTest.java
@@ -0,0 +1,19 @@
+package com.training.April16testing;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FileIOTest {
+	@Test
+	public void teststringreverse()
+
+	{
+		FileManipulation srt = new FileManipulation(); // Palindrome is tested
+
+	// assert statements
+	//assertEquals("true", srt.isReverse("Hello World"),true);
+	
+	}
+
+}
