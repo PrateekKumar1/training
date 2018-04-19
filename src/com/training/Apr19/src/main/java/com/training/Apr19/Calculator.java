f47bf884f6a8eb0c2f62a301cdbc049c62e507e9
PrateekKumar1 <38395576+PrateekKumar1@users.noreply.github.com>
2018-04-19 20:07:33 +0530

    Calculator Updated

--- src/com/training/Apr19/src/main/java/com/training/Apr19/Calculator.java
+++ src/com/training/Apr19/src/main/java/com/training/Apr19/Calculator.java
@@ -2,13 +2,13 @@
 * ********************************************************************************************************************************************************************** 
 *	Program name        : 	Calculator
 *	Program Description :	Compute the power of a number by implementing a calculator. Create class MyCalculator which consists of a single method long power(int,int)
-*							This method takes two integer, n and p as parameters and finds n^p. If either n or p is negative, then the method must through an exception 
-*							which says "n or p should not be zero". Also if both are zero, then the method must throw an exception which says "n and p should not be
-*							zero".
-*							Constraints
-*								-10<=n<=10
-*								-10<=p<=10
-*   Date				:	19-April-2018
+*				This method takes two integer, n and p as parameters and finds n^p. If either n or p is negative, then the method must through an exception 
+*				which says "n or p should not be zero". Also if both are zero, then the method must throw an exception which says "n and p should not be
+*				zero".
+*				Constraints
+*					-10<=n<=10
+*					-10<=p<=10
+*   Date		    :	19-April-2018
 *  ********************************************************************************************************************************************************************* 
 *
 */
@@ -28,11 +28,11 @@ public class Calculator {
 	 */
 	static void validate(int numbs,int powrs)throws NegativeZeroNumberException{  
 		if(numbs==0 && powrs==0)
-	    	 throw new NegativeZeroNumberException("n and p should not be zero.");  
+	    	   throw new NegativeZeroNumberException("n and p should not be zero.");  
 		else if(numbs<0 || powrs<0)  
-	      throw new NegativeZeroNumberException("n or p should not be negative.");  
-	     else  if(numbs==0 || powrs==0)
-	    	 throw new NegativeZeroNumberException("n or p should not be zero."); 
+	      		throw new NegativeZeroNumberException("n or p should not be negative.");  
+	    		 else  if(numbs==0 || powrs==0)
+	    	 		throw new NegativeZeroNumberException("n or p should not be zero."); 
 	     
 	    //  System.out.println("Valid number and power");  
 	   }  
