bfbc3c1cfe602dcd5ecc584082a8701ca4784eb7
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-20 19:43:51 +0530

    Bank PMD

--- src/com/training/Bank/src/main/java/com/training/bank/Address.java
+++ src/com/training/Bank/src/main/java/com/training/bank/Address.java
@@ -2,19 +2,115 @@
  * 
  */
 package com.training.bank;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
 /**
  * @author trainee
- *
+ *Class 'Address' contains scanner,logger classes.
+ *Contains flatno number, address, city, province and pincode.
  */
-public class Address {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Address {
+final static Scanner read=new Scanner(System.in);
+final static Logger lps=Logger.getLogger(Address.class.getName());
+private String Flatno;
+private	String address;
+private String city;
+private String Province_State;
+private int pincodes;
+/**
+ * @param Flatno
+ * @param address
+ * @param city
+ * @param Province_State
+ * @param pincodes
+ */
+public Address(final String Flatno,final String address,final String city,final String Province_State,int pincodes)
+{
+super();
+this.Flatno=Flatno;
+this.address=address;
+this.city=city;
+this.Province_State=Province_State;
+this.pincodes=pincodes;
+}
+/**
+ * @param Flatno
+ */
+public void setflat(final String Flatno){  
+this.Flatno=Flatno;  
+}
+/**
+ * @return
+ */
+public String getflat(){  
+return Flatno;  
+}  
+/**
+ * @param address
+ */
+public void setaddress(final String address){  
+this.address=address;  
+}
+/**
+ * @return
+ */
+public String getaddress(){  
+return address;  
+}    
+/**
+ * @param city
+ */
+public void setcity(final String city){  
+this.city=city;  
+}
+/**
+ * @return
+ */
+public String getcity(){  
+return city;  
+}  
+/**
+ * @param Province_State
+ */
+public void setprovince(final String Province_State){  
+this.Province_State=Province_State;  
+}
+/**
+ * @return
+ */
+public String getprovince(){  
+return Province_State;  
+}  	
+/**
+ * @param pincodes
+ * Takes pincode values
+ */
+public void setpincode(final int pincodes){  
+this.pincodes=pincodes;
+}
+/**
+ * @return
+ * pin codes
+ */
+public int getpincode(){  
+return pincodes;  
+}  	
+/**
+ * @param args
+ */
+public static void main(final String[] args) throws IOException {
+final List<Object> adr = new ArrayList<Object>();
+lps.info("Enter flat number\t");
+final int ph1=read.nextInt();
+lps.info("Enter address\t");
+final String adrs=read.next();
+lps.info("Enter city\t");
+final String ctys=read.next();
+lps.info("Enter email\t");
+final String eml1=read.next();
 	}
 
 }
--- src/com/training/Bank/src/main/java/com/training/bank/Contact.java
+++ src/com/training/Bank/src/main/java/com/training/bank/Contact.java
@@ -3,6 +3,7 @@
  */
 package com.training.bank;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,49 +14,77 @@ import java.util.logging.Logger;
  * @author trainee
  *
  */
-class Contact {
+class Contact extends Bank{
 //Adding logger lps for capturing logging information
 final static Scanner read=new Scanner(System.in);
 final static Logger lps=Logger.getLogger(Contact.class.getName());
 private int phone;
 private	int mobile;
 private String email;
-public Contact(int phone,int mobile,String email)
+/**
+ * @param phone
+ * @param mobile
+ * @param email
+ */
+public Contact(final int phone,final int mobile,final String email)
 {
 super();
 this.phone=phone;
 this.mobile=mobile;
 this.email=email;
 }
-public void setphone(int phone){  
+/**
+ * @param phone
+ */
+public void setphone(final int phone){  
 this.phone=phone;  
 }
+/**
+ * @return
+ */
 public int getphone(){  
 return phone;  
 }  
-public void setMobile(int mobile){  
+/**
+ * @param mobile
+ */
+public void setMobile(final int mobile){  
 this.mobile=mobile;  
 }
+/**
+ * @return
+ */
 public int getMobile(){  
 return mobile;  
 }    
-public void setEmail(String email){  
+/**
+ * @param email
+ */
+public void setEmail(final String email){  
 this.email=email;  
 }
+/**
+ * @return
+ */
 public String getEmail(){  
 return email;  
 }  
-public static void main(String[] args) {
+/**
+ * @param args
+ */
+public static void main(final String[] args) {
 lps.info("Enter phone number\t");
-int ph1=read.nextInt();
+final int ph1=read.nextInt();
 lps.info("Enter mobile number\t");
-int mob1=read.nextInt();
+final int mob1=read.nextInt();
 lps.info("Enter email\t");
-String eml1=read.next();
-List<Object> contact = new ArrayList<Object>();
+final String eml1=read.next();
+final List<Object> contact = new ArrayList<Object>();
 contact.add(eml1);
 contact.add(ph1);
 contact.add(mob1);
+lps.info(ph1+"\t"+mob1+"\t"+eml1);
+read.close();
 }
 
 }
