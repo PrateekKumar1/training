ad7904f6a181cf22a1bb4e60a541d8887118125c
PrateekKumar1 <38395576+PrateekKumar1@users.noreply.github.com>
2018-04-20 19:03:35 +0530

    FileIo

--- src/com/training/April16testing PMD Maven/src/main/java/com/training/April16testing/FileManipulation.java
+++ src/com/training/April16testing PMD Maven/src/main/java/com/training/April16testing/FileManipulation.java
@@ -13,43 +13,35 @@ import java.io.IOException;
 
 class FileManipulation
 {
- 
-	public void Readmyfile() throws IOException
-	{
-		FileReader fr = null;
-		try {
-			fr = new FileReader("./hello.txt");
-		} catch (FileNotFoundException e) {
- 
-			e.printStackTrace();
-		}    
-     int i;    
-     while((i=fr.read())!=-1)    
-     System.out.print((char)i);    
-     fr.close();   
-	}
- 
-	public void writemyfile() throws IOException
-	{
-		try{    
-           FileWriter fw=new FileWriter("./hello.txt");    
-           fw.write("Welcome to javaTpoint.");    
-           fw.close();    
-          }catch(Exception e){System.out.println(e);}    
-          System.out.println("Success...");    
-	}
- 
- 
-
-	public static void main(String args[]) {    
-		FileManipulation m = new FileManipulation();
-		try {
-			m.Readmyfile();
-			m.writemyfile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-   
-   
-    }  
+public void Readmyfile() throws IOException
+{
+FileReader fr = null;
+try {
+fr = new FileReader("./hello.txt");
+} catch (FileNotFoundException e) {
+ e.printStackTrace();
+}    
+ int i;    
+while((i=fr.read())!=-1)    
+System.out.print((char)i);    
+fr.close();   
+}
+public void writemyfile() throws IOException
+{
+try{    
+ FileWriter fw=new FileWriter("./hello.txt");    
+ fw.write("Welcome to javaTpoint.");    
+ fw.close();    
+ }catch(Exception e){System.out.println(e);}    
+ System.out.println("Success...");    
+}
+public static void main(String args[]) {    
+FileManipulation m = new FileManipulation();
+try {
+m.Readmyfile();
+m.writemyfile();
+} catch (IOException e) {
+e.printStackTrace();
+}
+}  
 }
