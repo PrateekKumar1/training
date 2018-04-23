f8d824b5c516544d7930c29f844b653292de5725
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-23 18:46:37 +0530

    Spring Dependency Injection

--- src/com/training/Apr23/src/main/java/com/training/springcore/Apr23/Customer.java
+++ src/com/training/Apr23/src/main/java/com/training/springcore/Apr23/Customer.java
@@ -3,18 +3,29 @@
  */
 package com.training.springcore.Apr23;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 /**
  * @author PRATEEK KR
  *
  */
 class Customer {
-private String fname;
-private String lname;
+final static Scanner read=new Scanner(System.in);
+final static Logger lps=Logger.getLogger(Customer.class.getName());
+private static String fname;
+private static String lname;
+Customer(String fname,String lname){
+this.fname=fname;
+this.lname=lname;
+}
 /**
  * @param args
  */
 public static void main(String[] args) {
-
+lps.info("Enter first name and last name\t");
+fname=read.next();
+lname=read.next();
 }
 
 }
