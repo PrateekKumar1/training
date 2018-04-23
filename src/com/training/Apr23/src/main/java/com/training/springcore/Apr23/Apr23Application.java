348b0d3f70d936dc11d6ccc198dcda93304d007e
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-23 18:15:02 +0530

    Spring Dependency Injection

---
+++ src/com/training/Apr23/src/main/java/com/training/springcore/Apr23/Apr23Application.java
@@ -0,0 +1,12 @@
+package com.training.springcore.Apr23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apr23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apr23Application.class, args);
+	}
+}
