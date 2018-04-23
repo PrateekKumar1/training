6bd959aca8f052e633d49ffe9b2856a9e2e7648e
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-23 18:29:02 +0530

    Spring Dependency Injection

---
+++ src/com/training/Apr23/src/main/java/com/training/springcore/Apr23/Customer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.training.springcore.Apr23;
+
+/**
+ * @author PRATEEK KR
+ *
+ */
+class Customer {
+private String fname;
+private String lname;
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+
+}
+
+}
