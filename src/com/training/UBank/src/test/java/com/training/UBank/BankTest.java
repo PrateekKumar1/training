747ee032e64ac17aad73a670806d476028b62d57
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-24 11:24:28 +0530

    Bank junit

--- src/com/training/UBank/src/test/java/com/training/UBank/BankTest.java
+++ src/com/training/UBank/src/test/java/com/training/UBank/BankTest.java
@@ -4,6 +4,7 @@
 package com.training.UBank;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -18,6 +19,31 @@ import org.junit.Test;
 public class BankTest {
 Customer cust=new Customer("Prateek", "Kumar",798132456,"abc.12@xmail.com","C-12","Vasant Kunj","Delhi",501234);
 SavingAccount sva=new SavingAccount(1300,cust);
+@Test
+public void nullfirstname() {
+String str=null;
+assertNotEquals("Null not allowed",cust.getfname());
+}
+@Test
+public void nulllasttname() {
+String str=null;
+assertNotEquals("Null not allowed",cust.getlname());
+}
+@Test
+public void nullemail() {
+String str=null;
+assertNotEquals("Null not allowed",cust.getEmail());
+}
+@Test
+public void nulladress() {
+String str=null;
+assertNotEquals("Null not allowed",cust.getAddress());
+}
+@Test
+public void nullpincodes() {
+int pin;
+assertNotEquals("Null not allowed",cust.getZip());
+}
 @Test
 public void testwithdrawpass() {
 double balance=16000;
