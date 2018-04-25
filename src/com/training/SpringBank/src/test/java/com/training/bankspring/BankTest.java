2accbbd1593b73cbd1843cd53a77b629d72c9fbc
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-25 17:24:07 +0530

    Spring Bank PMD junit

--- src/com/training/SpringBank/src/test/java/com/training/bankspring/BankTest.java
+++ src/com/training/SpringBank/src/test/java/com/training/bankspring/BankTest.java
@@ -16,55 +16,63 @@ import org.junit.Test;
  *
  */
 public class BankTest {
-Customers cust = null; // Calculator is tested
+Customers cust = null; 
 
 @Before
 public void setup()
 {
 cust=new Customers("Prateek", "Kumar",123,798132456,"abc.12@xmail.com","C-12","Vasant Kunj","Delhi","Delhi", 501234);
 } 
-
-//SavingAccount sva=new SavingAccount(1300,cust);
 @Test
 public void nullfirstname() {
-String str=null;
+final String str=null;
 assertNotEquals("Null not allowed",cust.getfname());
 }
 @Test
 public void nulllasttname() {
-String str=null;
+final String str=null;
 assertNotEquals("Null not allowed",cust.getlname());
 }
 @Test
 public void nullemail() {
-String str=null;
+final String str=null;
 assertNotEquals("Null not allowed",cust.getEmail());
 }
 @Test
+public void nullcity() {
+final String str=null;
+assertNotEquals("Null not allowed",cust.getcity());
+}
+@Test
 public void nulladress() {
-String str=null;
+final String str=null;
 assertNotEquals("Null not allowed",cust.getaddress());
 }
 @Test
+public void nullprovince() {
+final String province=null;
+assertNotEquals("Null not allowed",cust.getprovince());
+}
+@Test
 public void nullpincodes() {
-int pin;
+final int pin;
 assertNotEquals("Null not allowed",cust.getpincode());
 }
 @Test
 public void testwithdrawpass() {
 double balance=16000;
-double amount=1200;
+final double amount=1200;
 assertTrue((balance=balance-amount)==14800);
 }
 @Test
 public void testwithdrawfail() {
 double balance=16000;
-double amount=1200;
+final double amount=1200;
 assertTrue((balance=balance-amount)!=1800);
 }
 @Test
 public void testdeposit() {
-int i=400;
+final int i=400;
 double balance=1800;
 assertTrue((balance=balance+i)==2200);	
 }
