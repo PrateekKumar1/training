0fc3d15957ad1b398311b9372f4f505a491ea4c1
Prateek Kumar <prateekkr1295@gmail.com>
2018-04-26 11:57:48 +0530

    Bank account selection

--- src/com/training/BankMatrix/src/main/java/com/training/controller/App.java
+++ src/com/training/BankMatrix/src/main/java/com/training/controller/App.java
@@ -18,17 +18,49 @@ Bank bnks;
 Customers cust;
 public static void main( String[] args )
 {
-int ch;
+int choice;
+int choice2;
 displayname();
 lps.info("Enter the type of account");
 lps.info("1>Savings Account\n2>Flexible Account\n3>Checking Account\t");
-ch=read.nextInt();
-switch(ch)
+choice=read.nextInt();
+switch(choice)
 {
-case 1:break;
-case 2:break;
-case 3:break;
-default:lps.info("Invalid selection");
+case 1:lps.info("Savings Account");
+lps.info("Enter the operation you want to perform");
+lps.info("1>Withdrwal\n2>Deposit\t");
+choice2=read.nextInt();
+	switch(choice2) {
+	case 1:break;
+	case 2:break;
+	default:lps.info("Invalid operation selection!!!");
+	break;
+	}
+break;
+case 2:lps.info("Flexible Account");
+lps.info("Enter the operation you want to perform");
+lps.info("1>Withdrwal\n2>Deposit\t");
+choice2=read.nextInt();
+	switch(choice2) {
+	case 1:break;
+	case 2:break;
+	default:lps.info("Invalid operation selection!!!");;
+	break;
+	}
+	break;
+case 3:lps.info("Checking Account");
+lps.info("Enter the operation you want to perform");
+lps.info("1>Withdrwal\n2>Deposit\t");
+choice2=read.nextInt();
+	switch(choice2) {
+	case 1:break;
+	case 2:break;
+	default:lps.info("Invalid operation selection!!!");
+	break;
+	}
+	break;
+default:lps.info("Invalid Bank account selection selection");
 }
+read.close();
 }
 }
